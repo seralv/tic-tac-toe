cd110c5e40288aec6d29ff9c31cf63ce700a70d6
seralv <sergioalvaro.14@gmail.com>
2024-12-19 05:48:54 -0400

    Starting logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,33 @@
-console.log("Proofing");
+const MakeBoard = (function () {
+  let board = [];
+  return {
+    generate: function () {
+      for (let x = 0; x < 3; x++) {
+        for (let y = 0; y < 3; y++) {
+          board.push(`[${x}, ${y}]`);
+        }
+      }
+    },
+  };
+})();
+
+const Game = (function () {
+  // game code
+})();
+
+function createPlayer(playerName) {
+  return {
+    playerName: playerName,
+    greeting: function () {
+      console.log(`Hello I am ${this.playerName}`);
+    },
+  };
+}
+
+const firstPlayer = createPlayer("Bob");
+const secondPlayer = createPlayer("Tom");
+
+MakeBoard.generate();
+
+firstPlayer.greeting();
+secondPlayer.greeting();
