6f1d23a869586ae0d5541b4c6dfb968b133be624
seralv <sergioalvaro.14@gmail.com>
2024-12-20 12:42:58 -0400

    First fill the array

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,24 @@
 const MakeBoard = (function () {
   let board = [];
+  function showBoard() {
+    console.log(board);
+  }
   return {
     generate: function () {
       for (let x = 0; x < 3; x++) {
+        let row = [];
         for (let y = 0; y < 3; y++) {
-          board.push(`[${x}, ${y}]`);
+          // row.push(`[${x}, ${y}]`);
+          row.push(" ")
         }
+        board.push(row);
       }
+      showBoard();
     },
+    updateBoard: function(spaceX, spaceY, figure) {
+      board[spaceX][spaceX, spaceY] = figure;
+      showBoard();
+    }
   };
 })();
 
@@ -15,19 +26,26 @@ const Game = (function () {
   // game code
 })();
 
-function createPlayer(playerName) {
+function createPlayer(playerName, figure) {
   return {
     playerName: playerName,
+    figure: figure,
     greeting: function () {
       console.log(`Hello I am ${this.playerName}`);
     },
+    turnGame: function(spaceX, spaceY) {
+      MakeBoard.updateBoard(spaceX, spaceY, figure);
+    }
   };
 }
 
-const firstPlayer = createPlayer("Bob");
-const secondPlayer = createPlayer("Tom");
+const firstPlayer = createPlayer("Bob", "X");
+const secondPlayer = createPlayer("Tom", "O");
 
 MakeBoard.generate();
-
-firstPlayer.greeting();
-secondPlayer.greeting();
+// MakeBoard.updateBoard(0,1,"X")
+// MakeBoard.updateBoard(0,0,"O")
+firstPlayer.turnGame(0, 1);
+secondPlayer.turnGame(0, 0);
+firstPlayer.turnGame(1, 1);
+secondPlayer.turnGame(2, 1)
