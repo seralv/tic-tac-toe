722e94321dd46f58ecbe41779496209397394cc3
seralv <sergioalvaro.14@gmail.com>
2024-12-22 05:58:03 -0400

    Update turn condition

--- scripts/main.js
+++ scripts/main.js
@@ -9,16 +9,22 @@ const MakeBoard = (function () {
         let row = [];
         for (let y = 0; y < 3; y++) {
           // row.push(`[${x}, ${y}]`);
-          row.push(" ")
+          row.push(" ");
         }
         board.push(row);
       }
       showBoard();
     },
-    updateBoard: function(spaceX, spaceY, figure) {
-      board[spaceX][spaceX, spaceY] = figure;
-      showBoard();
-    }
+    updateBoard: function (spaceX, spaceY, figure, turn) {
+      console.log("first player:", firstPlayer);
+      console.log("second player:", secondPlayer);
+      if (board[spaceX][(spaceX, spaceY)] === " ") {
+        board[spaceX][(spaceX, spaceY)] = figure;
+        showBoard();
+      } else {
+        console.log("The space is full, change your option");
+      }
+    },
   };
 })();
 
@@ -26,26 +32,48 @@ const Game = (function () {
   // game code
 })();
 
-function createPlayer(playerName, figure) {
+const TurnController = {
+  currentPlayer: null,
+  toggleTurn: function (players) {
+    players.forEach((player) => {
+      player.turn = !player.turn;
+      if (player.turn) {
+        this.currentPlayer = player;
+      }
+    });
+  },
+};
+
+function createPlayer(playerName, figure, turn = false) {
   return {
     playerName: playerName,
     figure: figure,
+    turn: turn,
     greeting: function () {
       console.log(`Hello I am ${this.playerName}`);
     },
-    turnGame: function(spaceX, spaceY) {
-      MakeBoard.updateBoard(spaceX, spaceY, figure);
-    }
+    turnGame: function (spaceX, spaceY) {
+      if (this.turn) {
+        MakeBoard.updateBoard(spaceX, spaceY, this.figure, this.turn);
+        TurnController.toggleTurn(players);
+      } else {
+        console.log(`It's not ${this.playerName}'s turn!`);
+      }
+    },
   };
 }
 
 const firstPlayer = createPlayer("Bob", "X");
 const secondPlayer = createPlayer("Tom", "O");
 
+const players = [firstPlayer, secondPlayer];
+
 MakeBoard.generate();
-// MakeBoard.updateBoard(0,1,"X")
-// MakeBoard.updateBoard(0,0,"O")
 firstPlayer.turnGame(0, 1);
 secondPlayer.turnGame(0, 0);
 firstPlayer.turnGame(1, 1);
-secondPlayer.turnGame(2, 1)
+secondPlayer.turnGame(2, 1);
+firstPlayer.turnGame(0, 2);
+secondPlayer.turnGame(2, 2);
+firstPlayer.turnGame(0, 2);
+firstPlayer.turnGame(2, 0);
