5b7bbedb571d36fb6fa3a97e370042b33ee25fac
seralv <sergioalvaro.14@gmail.com>
2024-12-23 12:29:48 -0400

    Verify winner completed

--- scripts/main.js
+++ scripts/main.js
@@ -2,24 +2,38 @@ const MakeBoard = (function () {
   let board = [];
   let currentPlayer = null;
   function showBoard() {
-    console.log(board);
+    let counter = 0;
+    for (let x = 0; x < 3; x++) {
+      let row = []; // Crear una variable para acumular los valores de la fila
+
+      // Recorremos las columnas
+      for (let y = 0; y < 3; y++) {
+        row.push(board[counter]); // Agregar el valor de counter a la fila
+        counter++;
+
+        // Agregar un espacio si no es el último valor de la fila
+        // if (y < 2) {
+        //   row += " ";
+        // }
+      }
+      console.log(row); // Imprimir la fila
+    }
   }
   return {
     generate: function () {
+      // let counter = 0;
       for (let x = 0; x < 3; x++) {
-        let row = [];
         for (let y = 0; y < 3; y++) {
-          // row.push(`[${x}, ${y}]`);
-          row.push(" ");
+          // board.push(counter++);
+          board.push(" ");
         }
-        board.push(row);
       }
       showBoard();
     },
-    updateBoard: function (spaceX, spaceY, figure) {
-      if (board[spaceX][(spaceX, spaceY)] === " ") {
-        board[spaceX][(spaceX, spaceY)] = figure;
-        console.log("currentPlayer:", currentPlayer);
+    updateBoard: function (space, figure) {
+      if (board[space] === " ") {
+        board[space] = figure;
+        // console.log("currentPlayer:", currentPlayer);
         showBoard();
         this.verifyBoard();
         this.toggleTurn(players);
@@ -36,23 +50,30 @@ const MakeBoard = (function () {
       });
     },
     verifyBoard: function () {
-      for (let x = 0; x < board.length; x++) {
-        let vertical = [];
-        for (let y = 0; y < board.length; y++) {
-          vertical.push(board[y][(y, x)]);
-        }
-        if (vertical.every((value) => value === value[x])) {
-          console.log("vertical:", vertical);
-        }
+      const checkLine = (line) => line.every(value => value !== " " && value === line[0]);
+
+      const lines = [
+        [board[0], board[1], board[2]],  // Line 1
+        [board[3], board[4], board[5]],  // Line 2
+        [board[6], board[7], board[8]],  // Line 3
+        [board[0], board[3], board[6]],  // Column 1
+        [board[1], board[4], board[7]],  // Column 2
+        [board[2], board[5], board[8]],  // Column 3
+        [board[0], board[4], board[8]],  // Diagonal 1
+        [board[6], board[4], board[2]],  // Diagonal 2
+      ];
+
+      const winner = lines.some(checkLine);
+
+      if (winner) {
+        console.log(`${currentPlayer.playerName.toUpperCase()} WINS THE GAME`);
+        return;
       }
-      for (let i = 0; i < board.length; i++) {
-        if (
-          board[i].every((value) => value === board[i][(i, 0)] && value != " ")
-        ) {
-          console.log(
-            `${currentPlayer.playerName.toUpperCase()} WINS THE GAME`,
-          );
-        }
+
+      const isBoardFull = board.every(cell => cell !== " ")
+
+      if (isBoardFull) {
+        console.log("IT'S A TIE!!!")
       }
     },
   };
@@ -66,9 +87,10 @@ function createPlayer(playerName, figure, turn) {
     greeting: function () {
       console.log(`Hello I am ${this.playerName}`);
     },
-    turnGame: function (spaceX, spaceY) {
+    turnGame: function (space) {
       if (this.turn) {
-        MakeBoard.updateBoard(spaceX, spaceY, this.figure, this.turn);
+        console.log(`${this.playerName}'s turn`)
+        MakeBoard.updateBoard(space, this.figure);
       } else {
         console.log(`${this.playerName}, it's not your turn!`);
       }
@@ -76,18 +98,18 @@ function createPlayer(playerName, figure, turn) {
   };
 }
 
-const firstPlayer = createPlayer("Bob", "X", true);
-const secondPlayer = createPlayer("Tom", "O", false);
+const firstPlayer = createPlayer("Sergio", "X", true);
+const secondPlayer = createPlayer("Luciana", "O", false);
 
 const players = [firstPlayer, secondPlayer];
 
 MakeBoard.generate();
-firstPlayer.turnGame(0, 1);
-secondPlayer.turnGame(2, 0);
-secondPlayer.turnGame(1, 2);
-firstPlayer.turnGame(1, 1);
-secondPlayer.turnGame(0, 2);
-firstPlayer.turnGame(2, 1);
-secondPlayer.turnGame(2, 2);
-// firstPlayer.turnGame(0, 2);
-// firstPlayer.turnGame(0, 0);
+firstPlayer.turnGame(2);
+secondPlayer.turnGame(4);
+firstPlayer.turnGame(6);
+secondPlayer.turnGame(0);
+firstPlayer.turnGame(8);
+secondPlayer.turnGame(7)
+firstPlayer.turnGame(3);
+secondPlayer.turnGame(5)
+firstPlayer.turnGame(1);
