76e256c16021b7975c620c4f3b2c92da17d4253e
seralv <sergioalvaro.14@gmail.com>
2025-01-02 08:32:56 -0400

    Finished

--- scripts/main.js
+++ scripts/main.js
@@ -69,11 +69,6 @@ const MakeBoard = (function () {
       displayBoard.style.display = "grid";
       resetBtn.style.display = "inline";
       board = new Array(9).fill(" "); // Begin empty board
-      // for (let x = 0; x < 3; x++) {
-      //   for (let y = 0; y < 3; y++) {
-      //     board.push(" ");
-      //   }
-      // }
       showBoard();
     },
     updateBoard: function (space, figure) {
@@ -88,49 +83,6 @@ const MakeBoard = (function () {
         console.log("The space is full, change your option");
       }
     },
-    // toggleTurn: function () {
-    //   players.forEach((player) => {
-    //     player.turn = !player.turn;
-    //   });
-    //   currentPlayer = players.find((player) => player.turn);
-    //   spaceName.textContent = `${currentPlayer.playerName}'s turn`;
-    // },
-    // verifyBoard: function () {
-    //   const checkLine = (line) =>
-    //     line.every((value) => value !== " " && value === line[0]);
-    //
-    //   const lines = [
-    //     [board[0], board[1], board[2]], // Line 1
-    //     [board[3], board[4], board[5]], // Line 2
-    //     [board[6], board[7], board[8]], // Line 3
-    //     [board[0], board[3], board[6]], // Column 1
-    //     [board[1], board[4], board[7]], // Column 2
-    //     [board[2], board[5], board[8]], // Column 3
-    //     [board[0], board[4], board[8]], // Diagonal 1
-    //     [board[6], board[4], board[2]], // Diagonal 2
-    //   ];
-    //
-    //   const winner = lines.some(checkLine);
-    //
-    //   if (winner) {
-    //     console.log(`${currentPlayer.playerName.toUpperCase()} WINS THE GAME`);
-    //     spaceName.style.color = "#fd151b";
-    //     spaceName.textContent = `${currentPlayer.playerName.toUpperCase()} WINS THE GAME`;
-    //     displayBoard.style.pointerEvents = "none";
-    //     return;
-    //   } else {
-    //     this.toggleTurn();
-    //   }
-    //
-    //   const isBoardFull = board.every((cell) => cell !== " ");
-    //
-    //   if (isBoardFull) {
-    //     console.log("IT'S A TIE!!!");
-    //     spaceName.style.color = "#fd151b";
-    //     spaceName.textContent = "IT'S A TIE";
-    //     return;
-    //   }
-    // },
     verifyPlayers: function (nameFirstPlayer, nameSecondPlayer) {
       // if (nameFirstPlayer !== "" && nameSecondPlayer !== "") {
       if (nameFirstPlayer && nameSecondPlayer) {
@@ -153,11 +105,6 @@ const MakeBoard = (function () {
 // Create players factory
 function createPlayer(playerName, figure, turn) {
   return { playerName, figure, turn };
-  // return {
-  //   playerName: playerName,
-  //   figure: figure,
-  //   turn: turn,
-  // };
 }
 
 document.getElementById("start-btn").addEventListener("click", function () {
